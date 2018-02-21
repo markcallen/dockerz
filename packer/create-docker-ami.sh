9171241d81d354078d56416927eacf8cd3f13456
Mark C Allen <mark.allen@hootsuite.com>
2018-02-21 10:29:45 -0800

    Added license header

--- packer/create-docker-ami.sh
+++ packer/create-docker-ami.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 #
+# DockerZ create AMIs with packer
+#
+# https://github.com/markcallen/dockerz
+#
+# License & Authors
+#
+# Author:: Mark Allen (mark@markcallen.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
+
+path_to_docker=$(which docker)
+if [ ! -x "$path_to_docker" ] ; then
+   echo "Can't find docker, check that its in your path or download it from https://www.docker.com/get-docker"
+   exit 1;
+fi
 
 if ! docker history -q hashicorp/terraform:light >/dev/null 2>&1; then
  docker pull hashicorp/terraform:light
@@ -32,7 +57,7 @@ fi
 
 $TERRAFORM init -no-color /terraform
 
-$TERRAFORM apply \
+$TERRAFORM apply -auto-approve \
 	-var public_ip=${PUBLIC_IP} \
 	-state=/terraform/terraform.tfstate \
 	/terraform
